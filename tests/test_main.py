7883e9bc54822339699119809ff7521d19f84786
Marco Sievers de Almeida Ximit Gaia <im.ximit@gmail.com>
2023-04-15 11:41:07 -0300

    feat: add container and injection types

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from witch_doctor import WitchDoctor
+from witch_doctor import WitchDoctor, InjectionType
 
 import pytest
 
@@ -34,16 +34,32 @@ class Stub3FromABCClass:
         return c.sum(self.a, self.b)
 
 
+class Stub4FromABCClass(IStubFromABCClass):
+    def __init__(self, a: int):
+        self.a = a
+
+    def sum(self, a: int, b: int):
+        return a + b + self.a
+
+
 def test_register_invalid_interface():
     with pytest.raises(TypeError) as error:
-        WitchDoctor.register(IStubClass, Stub1FromABCClass)
+        WitchDoctor.register(IStubClass, Stub1FromABCClass, InjectionType.SINGLETON)
     assert error.value.args == ("Interface does not inherit from ABC",)
 
 
 def test_register_invalid_class_reference():
     with pytest.raises(TypeError) as error:
-        WitchDoctor.register(IStubFromABCClass, Stub2FromABCClass)
+        WitchDoctor.register(
+            IStubFromABCClass, Stub2FromABCClass, InjectionType.FACTORY
+        )
     assert error.value.args == ("Class reference not implements IStubFromABCClass",)
+
+
+def test_register_invalid_injection_type():
+    with pytest.raises(TypeError) as error:
+        WitchDoctor.register(IStubFromABCClass, Stub1FromABCClass, 3)
+    assert error.value.args == ("Invalid injection_type, must be one of InjectionType",)
 
 
 def test_injection_without_register():
@@ -61,7 +77,7 @@ def test_injection_without_register():
 
 
 def test_injection():
-    WitchDoctor.register(IStubFromABCClass, Stub1FromABCClass)
+    WitchDoctor.register(IStubFromABCClass, Stub1FromABCClass, InjectionType.FACTORY)
 
     @WitchDoctor.injection
     def func_t(a: int, b: int, c: IStubFromABCClass):
@@ -75,8 +91,33 @@ def test_injection():
 
 
 def test_injection_on_class_method():
-    WitchDoctor.register(IStubFromABCClass, Stub1FromABCClass)
+    WitchDoctor.register(IStubFromABCClass, Stub1FromABCClass, InjectionType.FACTORY)
 
     stub_c = Stub3FromABCClass(a=1, b=2)
     result = stub_c.perform_sum()
     assert result == 3
+
+
+def test_container_injection():
+    container = WitchDoctor.container("t1")
+    container(IStubFromABCClass, Stub4FromABCClass, InjectionType.SINGLETON, args=[20])
+    container = WitchDoctor.container()
+    container(IStubFromABCClass, Stub4FromABCClass, InjectionType.FACTORY, args=[10])
+
+    @WitchDoctor.injection
+    def func_t(a: int, b: int, c: IStubFromABCClass):
+        return c.sum(a, b)
+
+    result_a1 = func_t(a=1, b=2)
+    result_a2 = func_t(a=2, b=2)
+
+    assert result_a1 == 13
+    assert result_a2 == 14
+
+    WitchDoctor.load_container("t1")
+
+    result_a1 = func_t(a=1, b=2)
+    result_a2 = func_t(a=2, b=2)
+
+    assert result_a1 == 23
+    assert result_a2 == 24
--- witch_doctor/__init__.py
+++ witch_doctor/__init__.py
@@ -4,7 +4,20 @@ Witch Doctor a simple dependencies injection
 
 import inspect
 from abc import ABC
-from typing import Any, Type, TypeVar
+from enum import IntEnum
+from typing import Any, Type, TypeVar, List
+
+
+DEFAULT = "default"
+
+
+class InjectionType(IntEnum):
+    """
+    Available injections types
+    """
+
+    SINGLETON = 1
+    FACTORY = 2
 
 
 T = TypeVar("T")
@@ -16,14 +29,47 @@ class WitchDoctor:
     decorator to inject the dependencies
     """
 
-    __injection_map = {}
+    __injection_map = {"current": {}}
+    __singletons = {}
+
+    @classmethod
+    def container(cls, name: str = DEFAULT):
+        """
+        This method will provide a container register
+        :param name: Container name
+        :return: Wrapper for register func
+        """
+
+        def drugstore(
+            interface: Type[ABC],
+            class_ref: Any,
+            injection_type: InjectionType,
+            args: List[any] = None,
+        ):
+            cls.register(
+                interface=interface,
+                class_ref=class_ref,
+                injection_type=injection_type,
+                args=args,
+                container=name,
+            )
+
+        return drugstore
+
+    @classmethod
+    def load_container(cls, name: str = DEFAULT) -> None:
+        """
+        Will set the current container by the container name
+        :param name:
+        :return:
+        """
+        cls.__injection_map["current"].update(cls.__injection_map[name])
 
     @classmethod
     def injection(cls, function: T) -> T:
         """
         WitchDoctor.injection is a function decorator that will match the
         function params signature and inject the  dependencies.\n
-
         :type function: Callable
         """
 
@@ -33,23 +79,77 @@ class WitchDoctor:
                 if param in kwargs:
                     continue
                 param_type = signature.annotation
-                if class_ref := cls.__injection_map.get(param_type):
-                    kwargs.update({param: class_ref()})
+                if class_metadata := cls.__injection_map["current"].get(param_type):
+                    class_ref = class_metadata["cls"]
+                    class_args = class_metadata["args"]
+                    injection_type = class_metadata["injection_type"]
+                    instance = cls.__resolve_instance(
+                        class_ref, class_args, injection_type
+                    )
+                    kwargs.update({param: instance})
             return function(*args, **kwargs)
 
         medicine.__wrapped__ = function
         return medicine
 
     @classmethod
-    def register(cls, interface: Type[ABC], class_ref: Any):
+    def __resolve_instance(
+        cls, class_ref: T, args: list, injection_type: InjectionType
+    ) -> Type[T]:
+        if injection_type == InjectionType.SINGLETON:
+            if cls.__singletons.get(class_ref) is None:
+                cls.__singletons.update({class_ref: class_ref(*args)})
+            return cls.__singletons.get(class_ref)
+        if injection_type == InjectionType.FACTORY:
+            return class_ref(*args)
+        return None
+
+    @classmethod
+    def register(  # pylint: disable=R0913
+        cls,
+        interface: Type[ABC],
+        class_ref: Any,
+        injection_type: InjectionType,
+        args: List[any] = None,
+        container: str = DEFAULT,
+    ):
         """
         WitchDoctor.register will check inherit of the interface and class_ref.
         Will raise a TypeError on validation error\n
         :param interface: Interface that inherits from ABC
         :param class_ref: A implementation of the interface
+        :param injection_type: The injection type that must be used for this register.
+        Allowed Factory or Singleton
+        :param args: List of args tha will be used to instantiate the class object
+        :param container: Container name where the reference will be saved.
         """
         if not issubclass(interface, ABC):
             raise TypeError("Interface does not inherit from ABC")
         if not issubclass(class_ref, interface):
-            raise TypeError("Class reference not implements IStubFromABCClass")
-        cls.__injection_map.update({interface: class_ref})
+            raise TypeError(f"Class reference not implements {interface.__name__}")
+        if not isinstance(injection_type, InjectionType):
+            raise TypeError("Invalid injection_type, must be one of InjectionType")
+        if args is None:
+            args = []
+        if cls.__injection_map.get(container) is None:
+            cls.__injection_map.update({container: {}})
+        cls.__injection_map[container].update(
+            {
+                interface: {
+                    "cls": class_ref,
+                    "args": args,
+                    "injection_type": injection_type,
+                }
+            }
+        )
+        if container == DEFAULT:
+            cls.__injection_map["current"].update(
+                {
+                    interface: {
+                        "cls": class_ref,
+                        "args": args,
+                        "injection_type": injection_type,
+                    }
+                }
+            )
+        return cls
